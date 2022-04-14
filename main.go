03de475b4fb2896316d97d1887d15c3af79ddf13
Samuel Oliveira <samueldevbr@gmail.com>
2022-04-14 20:44:46 -0300

    initializing queue and workers

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
-	"fmt"
+	"github.com/samdevbr/gandalf/queue"
 	"github.com/samdevbr/gandalf/spec"
+	"github.com/samdevbr/gandalf/worker"
 	"log"
+	"runtime"
+	"sync"
 )
+
+var NumWorkers = runtime.NumCPU() * 2
 
 func main() {
 	specification, err := spec.New("gandalf.toml")
@@ -13,5 +18,29 @@ func main() {
 		log.Fatalln(err.Error())
 	}
 
-	fmt.Println(specification.PHP.Framework)
+	q := new(queue.Queue)
+
+	for _, scope := range specification.Scopes {
+		scopeFiles, err := scope.GetFiles()
+
+		if err != nil {
+			log.Println(err.Error())
+			continue
+		}
+
+		for _, file := range scopeFiles {
+			q.Push(file)
+		}
+	}
+
+	var group sync.WaitGroup
+
+	for i := 0; i < NumWorkers; i++ {
+		w := worker.New(q)
+
+		group.Add(1)
+		go w.Start(&group)
+	}
+
+	group.Wait()
 }
