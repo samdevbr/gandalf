bbcab82949f770f7d1b6ba6c69eff5159f649623
Samuel Oliveira <samueldevbr@gmail.com>
2022-04-14 13:36:03 -0300

    creating spec file validation errors

---
+++ spec/errors.go
@@ -0,0 +1,26 @@
+package spec
+
+import (
+	"errors"
+	"fmt"
+)
+
+var ErrSpecWithoutScopes = errors.New("specification without scopes")
+var ErrUnsupportedPhpVersion = errors.New("unsupported php version")
+var ErrSpecWithoutPhpSettings = errors.New("specification without php settings")
+
+var ErrScopeWithoutName = func(index int) error {
+	return errors.New(fmt.Sprintf("scope does not have a name: index %d", index))
+}
+
+var ErrScopeWithoutPath = func(index int) error {
+	return errors.New(fmt.Sprintf("scope does not have a path: index %d", index))
+}
+
+var ErrScopeWithoutIncludes = func(index int) error {
+	return errors.New(fmt.Sprintf("scope does not have includes: index %d", index))
+}
+
+var ErrScopeWithoutExcludes = func(index int) error {
+	return errors.New(fmt.Sprintf("scope does not have excludes: index %d", index))
+}
